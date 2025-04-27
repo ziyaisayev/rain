a14640f76931a1bd1ec5945613a45488c961c232
ziyaisayev <155613230+ziyaisayev@users.noreply.github.com>
2025-04-27 12:11:25 +0400

    project.py

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import csv
-
+import tkinter as tk 
+from tkinter import filedialog,messagebox,scrolledtext
 def load_rain_data(filename):
     total_rain = 0.0
     with open(filename, newline="", encoding="utf-8") as csvfile:
@@ -39,19 +40,38 @@ def distribute_rain(total_rain, villages, total_population):
 
     return distribution, reserved
 
-def main():
-    rain_file = "rain_data.csv"
-    village_file = "rain_data.csv" 
-    total_rain = load_rain_data(rain_file)
-    villages, total_population = load_village_data(village_file)
-    distribution, reserved = distribute_rain(total_rain, villages, total_population)
+def open_files():
+    rain_file = filedialog.askopenfilename(title="sellect rain_data.csv",filetypes=[("CSV Files","*.csv")])
+    village_file = filedialog.askopenfilename(title="sellect village_data.csv",filetypes=[("CSV Files","*.csv")])
+    if rain_file and village_file:
+        try:
+            total_rain = load_rain_data(rain_file)
+            villages, total_population = load_village_data(village_file)
+            distribution, reserved = distribute_rain(total_rain, villages, total_population)
+            output_text.delete("1.0",tk.END)
+            output_text.insert(tk.END,"water distribution to village")
+            for village, litres in distribution.items():
+                output_text.insert(tk.END, f"{village}: {litres} litres\n")
+            output_text.insert(tk.END, f"\nReserved Water: {reserved} litres\n")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
+    else:
+        messagebox.showwarning("warning","file is not choicen")
+root = tk.Tk()
+root.title("Rain Water Distribution")
 
-    print("\n🌧️ Water Distribution Report 🌧️")
-    print("-" * 40)
-    for village, amount in distribution.items():
-        print(f"{village:<20} ➔ {amount:>8.2f} L")
-    print("-" * 40)
-    print(f"\n💧 Reserved Rainwater: {reserved:.2f} L")
+root.geometry('600x500')
+root.configure(bg='#E6F2FF')
 
-if __name__ == "__main__":
-    main()
+title_label = tk.Label(root, text="Rain Water Distribution App", font=("Arial", 20, 'bold'), bg='#E6F2FF')
+title_label.pack(pady=10)
+
+button_frame=tk.Frame(root,bg="#3ba189")
+button_frame.pack()
+
+btn1=tk.Button(button_frame,text="datalari daxil edin",command=open_files,bg="#b8e0b8",font=("Arial",14))
+btn1.pack()
+
+output_text=scrolledtext.ScrolledText(root,width=70,height=20,font=("Arial",12))
+output_text.pack()
+root.mainloop()
